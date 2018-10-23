8342d6a816d7119fa527de346bc6d19f892e9681
Neal <>
2018-10-23 14:46:11 +0800

    add JS APIs

---
+++ JSAPI/JSAppSugar.js
@@ -0,0 +1,15 @@
+
+/*
+import a JSA class to use
+
+@param {[string]} classes JSA class names
+*/
+function $import(classes){}
+
+/*
+define a JSA class
+
+@param {string} className
+@param {object} define
+*/
+function $class(className,define){}
---
+++ JSAPI/jsa/Object.js
@@ -0,0 +1,21 @@
+
+/*
+jsa.Object is the root of JSA class hierarchy. 
+*/
+class jsa.Object{
+
+	/*
+	@return A jsa.Object which contains weak reference to the current object.
+	*/
+	weakObject(){}
+
+	/*
+	@return Is the current JSA object a weak reference object
+	*/
+	isWeak(){}
+
+	/*
+	@return get reference to the original object 
+	*/
+	self(){}
+}
